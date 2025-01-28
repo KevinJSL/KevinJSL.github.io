4edd5577284fe0f537d8bd36504f698295087af2
KevinJSL <kjl9300@g.rit.edu>
2025-01-27 23:54:06 -0500

    Added a new page for the second assignment and updated the UI

--- route.py
+++ route.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
-def home():
-    with open('./index.html', 'r') as file:
-        html_content = file.read()
-    return render_template_string(html_content)
+def index():
+    return render_template('index.html')
+
+@app.route('/cp1', methods = ['GET', 'POST'])
+def asnm1():
+    return render_template("./cp1.html")
+
+@app.route('/cp2', methods = ['GET', 'POST'])
+def asnm2():
+    return render_template("./cp2.html")
+
+@app.route('/online_acts', methods = ['GET', 'POST'])
+def oa():
+    return render_template("./online_acts.html")
 
 
 if __name__ == '__main__':
