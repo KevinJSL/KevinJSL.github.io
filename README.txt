769ad722aaf02cc2f3032cabc15d49a79ea7ed50
KevinJSL <kjl9300@g.rit.edu>
2025-09-12 22:28:29 -0400

    Fixed up the main page

--- README.txt
+++ README.txt
@@ -10,6 +10,12 @@ How to use:
     - try <Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force>
     if you are getting a permission error 
 - get imports: <pip install -r requirements.txt>
+
+For local development:
 - run route.py
 - Navigate to http://127.0.0.1:5000
 
+Actual Cite:
+- run route.py build
+- Upload the build directory to AWS s3 bucket
+- Navigate to www.kevinland.portfolio
--- route.py
+++ route.py
@@ -1,78 +1,78 @@
-from flask import Flask, render_template_string, render_template
+from flask import Flask, render_template
+from flask_frozen import Freezer   # <-- NEW
 
 app = Flask(__name__, static_url_path='/static')
+freezer = Freezer(app)             # <-- NEW
 
-
-# Index
+# ---------- ROUTES ----------
 @app.route('/')
 def index():
     return render_template("index.html")
 
-# School
-@app.route('/sw')
+@app.route('/sw/')
 def school_work():
     return render_template('school-work.html')
 
-# Animation
-@app.route('/a')
+@app.route('/a/')
 def animation():
     return render_template('animation/animation.html')
 
-@app.route('/a0')
+@app.route('/a0/')
 def a_a0():
     return render_template('animation/a0.html')
 
-# Global Illumination
-@app.route('/gi')
+@app.route('/gi/')
 def global_illumination():
     return render_template('global_illumination/global_illumination.html')
 
-@app.route('/cp1')
+@app.route('/cp1/')
 def asnm1():
     return render_template('global_illumination/cp1.html')
 
-@app.route('/cp2')
+@app.route('/cp2/')
 def asnm2():
     return render_template('global_illumination/cp2.html')
 
-@app.route('/cp3')
+@app.route('/cp3/')
 def asnm3():
     return render_template('global_illumination/cp3.html')
 
-@app.route('/cp4')
+@app.route('/cp4/')
 def asnm4():
     return render_template('global_illumination/cp4.html')
 
-@app.route('/cp5')
+@app.route('/cp5/')
 def asnm5():
     return render_template('global_illumination/cp5.html')
 
-@app.route('/cp6')
+@app.route('/cp6/')
 def asnm6():
     return render_template('global_illumination/cp6.html')
 
-@app.route('/cp7')
+@app.route('/cp7/')
 def asnm7():
     return render_template('global_illumination/cp7.html')
 
-@app.route('/KD')
+@app.route('/KD/')
 def assmKD():
     return render_template('global_illumination/KD.html')
 
-@app.route('/volume')
+@app.route('/volume/')
 def assmFinal():
     return render_template('global_illumination/volume.html')
 
-# Projects
-@app.route('/pp')
+@app.route('/pp/')
 def projects():
     return render_template('personal-projects.html')
 
-# Papers
-@app.route('/p')
+@app.route('/p/')
 def papers():
     return render_template('publications.html')
 
-
+# ---------- MAIN ----------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()   # <-- NEW: generate static site into ./build/
+    else:
+        app.run(debug=True)
